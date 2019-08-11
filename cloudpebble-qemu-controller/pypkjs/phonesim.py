f8360b6166018a0070ba33eddabbbdbf21e88e8f
Matt Rinsch <gfunkmonk@gmail.com>
2019-08-10 22:05:32 -0400

    update

--- cloudpebble-qemu-controller/pypkjs/phonesim.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-
-from pypkjs.runner.websocket import run_tool
-if __name__ == "__main__":
-    run_tool()
--- cloudpebble-qemu-controller/pypkjs/pypkjs/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import absolute_import
-__author__ = 'katharine'
-
-from gevent import monkey
-monkey.patch_all()
