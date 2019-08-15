263b76d4e34bf9f81a435761571089cac8069dfe
root <root@ip-172-31-68-254.ec2.internal>
2019-08-15 12:06:02 +0000

    fixes

--- dev_setup.sh
+++ dev_setup.sh
@@ -4,5 +4,6 @@ docker-compose build
 # Do this in the mounted directory, since the Dockerfile did it in a folder we
 # mask by mounting over it.
 docker-compose run web sh -c "rm -rf bower_components && cd /tmp && python /code/manage.py bower install && mv bower_components /code/"
+docker-compose run web sh -c "python manage.py collectstatic"
 # Stop everything again.
 docker-compose stop
